1972c8882fbfc2e5627ad71dc87df0f484a3e341
Jan <kupper.thorge@gmail.com>
2025-02-25 09:28:05 +0100

    q2 done

--- pages/question2.js
+++ pages/question2.js
@@ -4,12 +4,35 @@
 // När man klickar på knappen ska namnet byta plats med namnet framför i listan.
 // Det sista namnet i listan ska inte ha en knapp.
 
+// 2. Change location with callback function
+// Display the list of names by rendering a User component for each name.
+// The User component should have a button next to the name that says "move forward"
+// When the button is clicked, the name should change places with the name in front of the list.
+// The last name in the list should not have a button.
+
+import { useState } from 'react';
+
 export default function Question2() {
-  const [users, setUsers] = useState(["Fred", "Sven", "Anna", "Per"]);
+  const [users, setUsers] = useState(['Fred', 'Sven', 'Anna', 'Per']);
+  const [showIndex, setShowIndex] = useState(0);
+
+  function handleClick() {
+    setShowIndex(showIndex + 1);
+  }
 
   return (
     <div>
       <h1>Question 2</h1>
+      <User users={users} handleClick={handleClick} showIndex={showIndex} />
+    </div>
+  );
+}
+
+function User({ users, handleClick, showIndex }) {
+  return (
+    <div>
+      {users[showIndex]}
+      <div className='flex'>{showIndex != users.length - 1 && <button onClick={handleClick}>Next</button>}</div>
     </div>
   );
 }
