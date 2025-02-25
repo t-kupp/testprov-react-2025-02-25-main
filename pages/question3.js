1d0a6ea58511bb7248a6e892307f9815498f46a1
Jan <kupper.thorge@gmail.com>
2025-02-25 09:33:08 +0100

    q3 done

--- pages/question3.js
+++ pages/question3.js
@@ -11,6 +11,17 @@ export default function Question3() {
   return (
     <div>
       <h1>Question 3</h1>
+      <Card title='Välkommen'>Detta är en React-komponent!</Card>
+      <Card title='Info'>React gör det enkelt att bygga UI.</Card>
+    </div>
+  );
+}
+
+function Card({ title, children }) {
+  return (
+    <div className='border p-4 m-4 text-center flex flex-col border-black w-fit rounded'>
+      <h3 className='font-bold text-lg'>{title}</h3>
+      <p>{children}</p>
     </div>
   );
 }
